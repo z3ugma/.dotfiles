fcf98e396299b711269db4112910b1fb7cf7e386
Fred Turkington <fred@ensodata.com>
2019-11-15 13:35:18 -0600

    Adding Listening function to see network ports

--- bash/functions.sh
+++ bash/functions.sh
@@ -38,3 +38,13 @@ function lowercaseAllDirs () {
     fi
   done
 }
+
+listening() {
+    if [ $# -eq 0 ]; then
+        sudo lsof -iTCP -sTCP:LISTEN -n -P
+    elif [ $# -eq 1 ]; then
+        sudo lsof -iTCP -sTCP:LISTEN -n -P | grep -i --color $1
+    else
+        echo "Usage: listening [pattern]"
+    fi
+}
