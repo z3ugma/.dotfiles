ba288429cf51b8c72497de83612ed9291f64071c
Fred Turkington <z3ugma@users.noreply.github.com>
2021-10-23 09:40:00 -0500

    Moving to zsh
    commented out irrelevant bash details

---
+++ .zshrc
@@ -0,0 +1,11 @@
+eval "$(pyenv init -)"
+eval "$(rbenv init -)"
+
+. ~/.dotfiles/bash/functions.sh
+. ~/.dotfiles/bash/path.sh
+. ~/.dotfiles/bash/env.sh
+. ~/.dotfiles/bash/config.sh
+. ~/.dotfiles/bash/aliases.sh
+. ~/.dotfiles/bash/completions.sh
+. ~/.dotfiles/bash/prompt.sh
+. ~/.dotfiles/bash/options.sh
--- bash/options.sh
+++ bash/options.sh
@@ -1,14 +1,5 @@
-# Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob;
+HISTSIZE=99999
+HISTFILESIZE=999999
+SAVEHIST=$HISTSIZE
 
-# Append to the Bash history file, rather than overwriting it
-shopt -s histappend;
-
-# Autocorrect typos in path names when using `cd`
-shopt -s cdspell;
-
-#History file details
-export HISTTIMEFORMAT="%h %d %H:%M:%S "
-export HISTSIZE=10000
-export HISTCONTROL=ignorespace:erasedups
-export HISTIGNORE="ls:history"
+alias history="history 1"
